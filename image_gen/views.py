1abd66e2ca1e56446d3c254b673795aee1eddc43
unknown <ayushpoddar586@gmail.com>
2023-12-10 01:37:17 +0530

    Ten

--- image_gen/views.py
+++ image_gen/views.py
@@ -3,9 +3,12 @@ from .utils import generate_image
 
 def generate_image_home(request):
     if request.method == 'POST':
-        print("hello")
-        prompt = request.POST.get('prompt', '')  # Get the prompt from the form
-        generated_image_path = generate_image(prompt)
-        return render(request, 'generate_image_home.html', {'generated_image_path': generated_image_path})
+        prompt = request.POST.get('prompt', '')
+        try:
+            generated_image_path = generate_image(prompt)
+            return render(request, 'generate_image_home.html', {'generated_image_path': generated_image_path})
+        except OSError as e:
+            error_message = f"Image generation failed: {e}"
+            return render(request, 'generate_image_home.html', {'error_message': error_message})
     else:
         return render(request, 'generate_image_home.html', {'generated_image_path': None})
